9073d670796d22b36e2828a9ded12e25bce63f74
aakashsell <aakashsell@gmail.com>
2021-01-06 17:58:36 -0500

    redo

--- aSellBlackjack/src/main/java/com/mycompany/asellblackjack/Deck.java
+++ aSellBlackjack/src/main/java/com/mycompany/asellblackjack/Deck.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.mycompany.asellblackjack;
 
 /**
  *
  * @author aakas
  */
-public class Player {
+public class Deck {
     
 }
